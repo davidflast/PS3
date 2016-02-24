5a24b1aa22a74d3e46a63fb1a6d25bd106b4fa2c
davidflast <davidflasterstein@wustl.edu>
2016-02-23 23:20:08 -0600

    Functionalized selecting the door

--- PS3.R
+++ PS3.R
@@ -2,6 +2,13 @@
 # David Flasterstein
 
 # 1. S3 object system
-doorChoice <- sample(1:3, 1)
+door_select <- function(doorChoice){
+if(doorChoice > 3 | doorChoice < 1){
+  return("That's not a door number")
+}
 class(doorChoice) <- "door"
+return(doorChoice)
+}
+
+
 
