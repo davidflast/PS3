bc90436996b9476dd09e403bbf71c75f5fb36bb2
davidflast <davidflasterstein@wustl.edu>
2016-02-24 02:46:28 -0600

    Creates PlayGame method

--- PS3-S4-Object.R
+++ PS3-S4-Object.R
@@ -1,9 +1,9 @@
-#PS3
+# PS3
 # David Flasterstein
 # S4 Object System
-rm(list=ls())
-library(methods)
 
+# creates S4 object of class "door", which allows the user to pick
+# a door numbered one, two, or three
 setClass(Class = "door", 
          representation = representation(doorNum = "numeric")
          )
@@ -15,8 +15,34 @@ setValidity("door", function(object){
     }
   )
 
+# Creates a generic "PlayGame"
+setGeneric("PlayGame",
+           function(object = "door"){
+            standardGeneric("PlayGame") 
+           })
+# Function to check if your door was the car
+#
+# This function checks to see if the users choice of door was
+# the door with the car, or with the goat
+# @param door: an object of class door
+# @return method prints whether you won the car, 
+#         or the goat with the door number for the car
+# @author David Flasterstein
+setMethod("PlayGame", "door",
+          function(object){
+            car <- sample(1:3, 1)
+            if(object@doorNum == car){
+              print("You won a Car!")
+            } else {
+              print(paste("Sorry, you won a goat, the car was actually in door", as.character(car)))
+            }
+          }
+          )
 
-new("door", doorNum =4)
+choice = new("door", doorNum = 3)
+PlayGame(choice)
+
+
 
 
 
